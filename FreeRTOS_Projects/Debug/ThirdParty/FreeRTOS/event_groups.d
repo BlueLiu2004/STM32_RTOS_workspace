0129a95e2e06a1f68d4ded0deec3300444ce832b
BlueLiu2004 <BlueLiu2004@gmail.com>
2025-03-30 23:29:18 +0800

    init commit

---
+++ FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/event_groups.d
@@ -0,0 +1,27 @@
+ThirdParty/FreeRTOS/event_groups.o: ../ThirdParty/FreeRTOS/event_groups.c \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/event_groups.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/event_groups.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h:
---
+++ FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/timers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/stack_macros.h:
