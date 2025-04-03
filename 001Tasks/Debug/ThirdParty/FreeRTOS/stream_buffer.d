3445cd81515e362641e9285fd60a40ebe3de82d9
BlueLiu2004 <BlueLiu2004@gmail.com>
2025-04-03 16:45:21 +0800

    建立了兩個task，更改FreeRTOSconfig.h和使用taskYIELD()取消搶占式排程，避免尚未印出全部資料時被切換至另一個task，導致輸出文字凌亂

---
+++ 001Tasks/Debug/ThirdParty/FreeRTOS/stream_buffer.d
@@ -0,0 +1,22 @@
+ThirdParty/FreeRTOS/stream_buffer.o: \
+ ../ThirdParty/FreeRTOS/stream_buffer.c \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/stream_buffer.h
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/stream_buffer.h:
---
+++ 001Tasks/Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/timers.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/stack_macros.h
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/timers.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Blue/OneDrive\ -\ GoldCowON\ Ltd/My_Code/STM32/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/stack_macros.h:
